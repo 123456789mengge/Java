883081183c456624b60a5b42c73b27de322cf279
123456789mengge <55129473+123456789mengge@users.noreply.github.com>
2019-10-08 20:39:51 +0800

    第四次实验作业

---
+++ 第四次实践作业/Kecheng课程包/src/Mainclass.java
@@ -0,0 +1,13 @@
+import course.Course;
+import teacher.Teacher;
+import student.Student;
+
+public class Mainclass {
+
+	public static void main(String[] args) {
+		Course co=new Course();
+		Student st=new Student();
+		Teacher te=new Teacher();
+
+	}
+}

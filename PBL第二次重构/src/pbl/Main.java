6e82a578ce4589668371a9874cbefb689b2b0fc6
Your Name <123456789mengge>
2019-12-03 21:10:13 +0800

    第二次重构

---
+++ PBL第二次重构/src/pbl/Main.java
@@ -0,0 +1,11 @@
+package pbl;
+
+public class Main {
+	 public static void main(String[] args) {
+		 Customer customer=new Customer("用户");
+			customer.addRental(new Rental(new Movie("《冰雪奇缘2》",0),5));
+			customer.addRental(new Rental(new Movie("《少年的你》",1),6));
+			customer.addRental(new Rental(new Movie("《中国机长》",2),7));
+		     System.out.println("租用详清："+"\n"+customer.statemant());
+     }
+}
